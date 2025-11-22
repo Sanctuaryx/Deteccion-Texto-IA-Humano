42388dc04c480afffce7be605b2709caa4d259ea
Raul <raulrj@usal.es>
2025-11-22 20:29:22 +0100

    clean

--- iatd/utils/calibration.py
+++
@@ -1,4 +0,0 @@
-from sklearn.calibration import CalibratedClassifierCV
-
-def calibrate_if_needed(clf, method='sigmoid', cv=3, enabled=True):
-    return CalibratedClassifierCV(clf, method=method, cv=cv) if enabled else clf
